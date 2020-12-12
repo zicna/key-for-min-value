1af841a83ecfcf4d7847e3b753f7bfce48e1198b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-11 23:45:59 -0600

    commit tree a185ddbd81414353293896e16609a73d6c824693

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,30 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# def key_for_min_value(name_hash)
+#     keys_arr = []
+#     if name_hash == {}
+#         return nil
+#     else
+#         keys_arr = name_hash.collect {|key, value| value}
+#     end
+#     keys_arr.min(1)
+# end
 
+def key_for_min_value(name_hash)
+    #should be min = +infinity
+    min = 10000
+    min_key = ""
+    if name_hash == {}
+        return nil
+    else
+        keys_arr = name_hash.each do |key, value|
+            if min > value
+                min_key = key
+                min = value
+            end
+        end
+        return min_key
+    end
+    
 end
